7a68bb0fdaff5312f7316d06f91b122658b0c027
Kenty Akoarum <kamome-black@akoarum.net>
2022-04-20 09:23:01 +0900

    add publish env

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const TerserPlugin = require('terser-webpack-plugin')
 const webpack = require('webpack')
+const pkg = require('./package.json')
 
 module.exports = {
   mode: 'production',
@@ -28,12 +29,15 @@ module.exports = {
   optimization: {
     minimizer: [
       new TerserPlugin({
-        extractComments: 'all',
+        extractComments: false,
         terserOptions: { compress: { drop_console: true } },
       }),
     ],
   },
   plugins: [
+    new webpack.BannerPlugin(
+      `Spearly CMS jQuery Plugin\n\nCopyright 2022 unimal Co.,Ltd.\nLicensed under ${pkg.license}`
+    ),
     new webpack.ProvidePlugin({
       $: 'jquery',
     }),
